d549b0ad675381a976b974675070100745607711
favour-droid <ngayifavournforshi@gmail.com>
2025-12-04 15:23:23 +0100

    goo

---
+++ app/components/UserNav.tsx
@@ -0,0 +1,56 @@
+/* eslint-disable @next/next/no-img-element */
+import { DropdownMenu, 
+     DropdownMenuItem, 
+     DropdownMenuTrigger ,
+        DropdownMenuContent
+    } from "@/components/ui/dropdown-menu";
+import { MenuIcon } from "lucide-react";
+import {
+    RegisterLink,
+    LoginLink,
+    LogoutLink
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+export async function UserNav() {
+    const {getUser}= getKindeServerSession();
+    const user = await getUser();
+
+    return (
+    <DropdownMenu>
+        <DropdownMenuTrigger>
+            <div className="rounded-full border px-2 py-2 lg:py-4 lg:px-4 flex items-center gap-3">
+               <MenuIcon className="w-6 h-6 lg:w-5 lg:h-5" /> 
+               <img 
+               src={
+                user?.picture??
+               "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
+                }
+               alt="Image of the user"
+               className="rounded-full h-8 w-8 hidden lg:block"/>
+                </div>
+
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-[200px]">
+        {user ? (
+            <>
+                <DropdownMenuItem>
+                <LogoutLink className="w-full">Logout</LogoutLink>
+                </DropdownMenuItem>
+            </>
+         ): (
+            <>
+            <DropdownMenuItem>
+                <RegisterLink className="w-full">Register</RegisterLink>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+                <LoginLink className="w-full">Login</LoginLink>
+                </DropdownMenuItem>
+                </>
+        )}
+
+        </DropdownMenuContent>
+
+    </DropdownMenu>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+
+import { MapFilterItems } from "./components/MapFilterItems";
 
 export default function Home() {
   return (
-    <Button>Hello from the index page</Button>
-  )
+    <div className="container mx-auto px-5 lg:px-10">
+      <MapFilterItems />
+    </div>
+  );
 }
